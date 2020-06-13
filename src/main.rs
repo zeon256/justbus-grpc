a479ec088bd8a4025764982c20eb4556d3cc820f
BudiNverse <me@inve.rs>
2020-06-13 09:56:15 +0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,68 @@
+#[macro_use]
+extern crate lazy_static;
+
+use crate::models::buses::just_bus_server::{JustBus, JustBusServer};
+use crate::models::buses::*;
+use crate::cache::Cache;
+use tonic::{Request, Response, Status};
+use lta::prelude::*;
+use lta::r#async::bus::get_arrival;
+use lta::r#async::lta_client::LTAClient;
+use std::env::var;
+use tonic::transport::Server;
+use std::time::Duration;
+
+mod models;
+mod cache;
+
+lazy_static! {
+    static ref CLIENT: LTAClient = {
+        let api_key = var("API_KEY").expect("API_KEY not found!");
+        LTAClient::with_api_key(api_key)
+    };
+
+    static ref CACHE: Cache<u32, BusTimings> = {
+        let duration = Duration::from_secs(15);
+        Cache::with_ttl(duration)
+    };
+}
+
+
+#[derive(Default)]
+pub struct MyJustBus;
+
+#[tonic::async_trait]
+impl JustBus for MyJustBus {
+    async fn timings(&self, request: Request<BusStopId>) -> Result<Response<BusTimings>, Status> {
+        let bus_stop_id = request.into_inner().bus_stop_id;
+        let in_cache = CACHE.get(bus_stop_id);
+        match in_cache {
+            Some(v) => Ok(Response::new(v)),
+            None => {
+                let res = get_arrival(&CLIENT, bus_stop_id, None)
+                    .await
+                    .map_err(|_| Status::internal("Client Error!"))?
+                    .services;
+
+                let data = BusTimings::from(res);
+                CACHE.insert(bus_stop_id, data.clone());
+
+                Ok(Response::new(data))
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    println!("Started server on {:?}", &addr);
+
+    let just_bus = MyJustBus::default();
+    Server::builder()
+        .add_service(JustBusServer::new(just_bus))
+        .serve(addr)
+        .await?;
+
+    Ok(())
+}
---
+++ src/models.rs
@@ -0,0 +1,81 @@
+use crate::models::buses::bus_timings::arrival_bus_service::NextBus;
+use crate::models::buses::bus_timings::ArrivalBusService;
+use crate::models::buses::BusTimings;
+use lta::models::bus::bus_arrival;
+use lta::models::bus_enums;
+
+pub mod buses {
+    tonic::include_proto!("buses");
+}
+
+impl From<Vec<bus_arrival::ArrivalBusService>> for BusTimings {
+    fn from(arrivals: Vec<bus_arrival::ArrivalBusService>) -> Self {
+        BusTimings {
+            arrivals: arrivals.into_iter().map(|v| From::from(v)).collect()
+        }
+    }
+}
+
+impl From<bus_arrival::ArrivalBusService> for ArrivalBusService {
+    fn from(arrivals: bus_arrival::ArrivalBusService) -> Self {
+        ArrivalBusService {
+            service_no: arrivals.service_no,
+            operator: from_bus_operator(&arrivals.operator),
+            next_bus: arrivals
+                .next_bus
+                .to_vec()
+                .into_iter()
+                .filter_map(|v| v.map(|v2| NextBus::from(v2)))
+                .collect(),
+        }
+    }
+}
+
+impl From<bus_arrival::NextBus> for NextBus {
+    fn from(nb: bus_arrival::NextBus) -> Self {
+        NextBus {
+            origin_code: nb.origin_code,
+            dest_code: nb.dest_code,
+            est_arrival: nb.est_arrival.timestamp(),
+            lat: nb.lat,
+            long: nb.long,
+            visit_no: nb.visit_no,
+            bus_load: from_bus_load(&nb.load),
+            feature: from_bus_feature(&nb.feature),
+            bus_type: from_bus_type(&nb.bus_type),
+        }
+    }
+}
+
+fn from_bus_feature(bf: &Option<bus_enums::BusFeature>) -> Option<i32> {
+    match bf {
+        Some(_) => Some(1),
+        _ => None,
+    }
+}
+
+fn from_bus_load(bl: &bus_enums::BusLoad) -> i32 {
+    match bl {
+        bus_enums::BusLoad::SeatsAvailable => 0,
+        bus_enums::BusLoad::StandingAvailable => 1,
+        bus_enums::BusLoad::LimitedStanding => 2,
+    }
+}
+
+fn from_bus_type(bt: &bus_enums::BusType) -> i32 {
+    match bt {
+        bus_enums::BusType::SingleDecker => 0,
+        bus_enums::BusType::DoubleDecker => 1,
+        bus_enums::BusType::Bendy => 2,
+    }
+}
+
+fn from_bus_operator(bo: &bus_enums::Operator) -> i32 {
+    match bo {
+        bus_enums::Operator::SBST => 0,
+        bus_enums::Operator::SMRT => 1,
+        bus_enums::Operator::TTS => 2,
+        bus_enums::Operator::GAS => 3,
+    }
+}
+
